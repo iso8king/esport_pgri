d70a3f75870a6c142e045dca06d86c86009819ea
Ridho Firdaus <ridhofirdaus321@gmail.com>
2026-06-23 23:28:23 +0700

    fixing bug in multipart request

--- src/routes/User/User_absensi.svelte
+++ src/routes/User/User_absensi.svelte
@@ -237,7 +237,7 @@
   // Kirim absen ke backend
   async function submitAbsenToBackend(kegiatanId, formData) {
     try {
-      const response = await fetchWithAuth(`/api/absen/${kegiatanId}/create`, {
+      const response = await fetch(`/api/absen/${kegiatanId}/create`, {
         method: 'POST',
         body: formData,
         credentials: 'include'
